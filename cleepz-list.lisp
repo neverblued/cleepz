b963bc3602de7138e998bf9b20e7076e2f9a8e0f
Demetrius Conde <condemetrius@gmail.com>
2010-12-19 23:35:30 +0200

    upd: List now can fill various slots.

--- cleepz-list.lisp
+++ cleepz-list.lisp
@@ -14,14 +14,15 @@
 (defgeneric templates-for-items (list-template list-data)
   (:documentation "Get a list templates suitable for viewing the list-data."))
 
-(defmethod view ((list-template list-template) &optional (data-list nil))
-  (let* ((items-templates (templates-for-items list-template data-list))
+(defmethod view ((list-template list-template) &optional (data nil))
+  (let* ((data-list (getf data :items))
+         (items-templates (templates-for-items list-template data-list))
          (items (mapcar (lambda (item-template data-record)
                           (view item-template data-record))
                         items-templates data-list)))
     (if (plusp (length items))
         (patch-slots (template-markup list-template)
-                     (list :items (apply #'join items)))
+                     (append (list :items (apply #'join items)) data))
         (view (clip-template (fetch-clip list-template :empty))))))
 
 (defmethod templates-for-items ((list-template list-template) list-data)
