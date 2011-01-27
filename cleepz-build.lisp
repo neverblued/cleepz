fb64e78c22ada37e512feff110479a357b4c7c5d
Demetrius Conde <condemetrius@gmail.com>
2011-01-28 00:55:53 +0200

    Данные шаблонов теперь через пакет вместо хэша.

--- cleepz-build.lisp
+++ cleepz-build.lisp
@@ -2,13 +2,23 @@
 
 ;; data
 
-(defparameter template-data (make-hash-table))
+(defpackage #:cleepz-data)
 
-(defun template-datum (name)
-  (gethash name template-data nil))
+(defmacro datum (symbol)
+  (with-gensyms (datum-symbol)
+    `(let ((,datum-symbol (find-symbol (symbol-name ',symbol) 'cleepz-data)))
+       (when (boundp ,datum-symbol)
+         (symbol-value ,datum-symbol)))))
 
-(defun (setf template-datum) (value name)
-  (setf (gethash name template-data) value))
+(defmacro with-datum ((symbol value) &body body)
+  `(progv
+       (list (intern (symbol-name ,symbol) 'cleepz-data))
+       (list ,value)
+     ,@body))
+
+(defun requested (data-key)
+  (declare (special request))
+  (getf request data-key))
 
 ;; behaviour
 
@@ -27,13 +37,14 @@
   (let ((parts nil)
         (length (length (view-source view)))
         (iterator 0))
+    (declare (special iterator))
     (macrolet ((join-clips ((var list) &body builder)
                  `(apply #'join (reverse (dolist (,var ,list parts) ,@builder))))
                (add-clip (&rest mode)
                  `(push (parse-string (clip view (list ,@mode))) parts)))
       (join-clips (value (view-source view))
         (incf iterator)
-        (setf (template-datum (view-item-var view)) value)
-        (add-clip :clip :item :iterator iterator)
+        (with-datum ((view-item-var view) value)
+          (add-clip :clip :item :iterator iterator))
         (when (< iterator length)
           (add-clip :clip :separator :iterator iterator))))))
